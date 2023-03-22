100fa285a02b2e952e23337b4ce4065116a79c88
jihongni <jihongni@stanford.edu>
2023-03-22 03:57:23 -0700

    added mock data visual with color blocks

--- SanaApp/components/HomeScreen/HomeScreen.js
+++ SanaApp/components/HomeScreen/HomeScreen.js
@@ -1,6 +1,10 @@
 import { StyleSheet, ScrollView, SafeAreaView, Button, Text, View } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
+import { ContributionGraph } from "react-native-chart-kit";
+import { Dimensions } from "react-native";
+const screenWidth = Dimensions.get("window").width;
+
 const _GetInfo = async () => {
     let userData = await AsyncStorage.getItem('userData');
     userData = JSON.parse(userData);
@@ -14,11 +18,11 @@ _GetInfo();
 
 const styles = StyleSheet.create({
     header: {
-        height: 150,
+        height: 120,
         width: '100%',
         padding: 40,
         backgroundColor:'#EAABFA',
-        paddingTop: 80,
+        paddingTop: 30,
         borderColor: 'black',
         borderradius: 3,
     },
@@ -56,6 +60,31 @@ const styles = StyleSheet.create({
     }
 });
 
+const chartConfig = {
+    backgroundGradientFrom: 'rgba(212, 132, 232, 0.89)',
+    backgroundGradientFromOpacity: 100,
+    backgroundGradientTo: 'rgba(113, 189, 244, 0.93)',
+    backgroundGradientToOpacity: 100,
+    color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+    strokeWidth: 2, // optional, default 3
+    barPercentage: 0.5,
+    useShadowColorFromDataset: false // optional
+  };
+
+const commitsData = [
+    { date: "2017-01-02", count: 1 },
+    { date: "2017-01-03", count: 2 },
+    { date: "2017-01-04", count: 3 },
+    { date: "2017-01-05", count: 4 },
+    { date: "2017-01-06", count: 5 },
+    { date: "2017-01-30", count: 2 },
+    { date: "2017-01-31", count: 3 },
+    { date: "2017-03-01", count: 2 },
+    { date: "2017-04-02", count: 4 },
+    { date: "2017-03-05", count: 2 },
+    { date: "2017-02-30", count: 4 }
+  ];
+
 export default function HomeScreen( { navigation }) {
     
     return (
@@ -66,13 +95,14 @@ export default function HomeScreen( { navigation }) {
             </View>
             <ScrollView>
                 <SafeAreaView> 
-                    <View style={styles.dataBox}>
-                        
-                    </View>
-                    <View style={styles.dataBox}></View>
-                    <View style={styles.dataBox}></View>
-                    <View style={styles.dataBox}></View>
-                    <View style={styles.dataBox}></View>
+                    <ContributionGraph
+                        values={commitsData}
+                        endDate={new Date("2017-04-01")}
+                        numDays={105}
+                        width={screenWidth}
+                        height={220}
+                        chartConfig={chartConfig}
+                    />
                 </SafeAreaView>
                 
             </ScrollView>
